eb22e51b115ee7f9004b0c3bff3e1fd283c22152
Berkus Decker <berkus+github@metta.systems>
2025-02-17 09:58:52 +0200

    wip add repak_from_json example

---
+++ examples/assets/README.md
@@ -0,0 +1,3 @@
+lenna.png - the reference "image"
+waves.mp4 - free 1080p video from https://www.pexels.com/ by Michal Marek
+teapot.fbx - a 3d model from [Freepik](https://www.freepik.com/3d-model/teapot-021_6741.htm#query=teapot&position=13&from_view=search&uuid=8ecc8d97-bb70-40fb-8d9f-169a1c5658c1)
---
+++ examples/repak_from_json/main.rs
@@ -0,0 +1,64 @@
+///
+/// Example of creating and updateing a REPAK file using a JSON manifest.
+///
+/// 1. deser json list of files
+/// 2. open or create a repak file
+/// 3. add files not present in the repak file
+///
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GlobalOptions {
+    compression: Compression,
+    encryption: Encryption,
+    checksums: Vec<Checksum>,
+}
+
+// @todo take these from REPAK itself?
+enum Compression {
+    #[serde(name = "best")]
+    Best,
+    #[serde(name = "none")]
+    None,
+}
+
+// @todo take these from REPAK itself?
+enum Encryption {
+    #[serde(name = "none")]
+    None,
+}
+
+// @todo take these from REPAK itself?
+enum Checksum {
+    #[serde(name = "sha3")]
+    Sha3,
+    K12,
+}
+
+#[derive(Deserialize)]
+struct Asset {
+    path: PathBuf,
+    name: String,
+    compression: Option<Compression>,
+    encryption: Option<Encryption>,
+    checksums: Option<Vec<Checksum>>,
+}
+
+#[derive(Deserialize)]
+struct Manifest {
+    global_options: GlobalOptions,
+    assets: Vec<Asset>,
+}
+
+/// Create or update a REPAK library from JSON manifest.
+#[derive(FromArgs)]
+struct Args {
+    #[positional]
+    manifest_file: PathBuf,
+}
+
+#[throws]
+fn main() {
+    let args = Args::from_env();
+    let m: Manifest = serde_json::from_reader(File::open(args.manifest_file)?)?;
+}
